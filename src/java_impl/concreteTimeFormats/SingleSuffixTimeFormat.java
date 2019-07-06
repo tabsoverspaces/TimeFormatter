0d5a7b7ccb98f2b50253bcf339ae1e3441998225
tabsoverspaces <d.postolovski@gmail.com>
2019-07-06 20:10:59 +0200

    Modified the single suffix format so that zeroes would be prepended to any time unit values smaller than 10.

--- src/java_impl/concreteTimeFormats/SingleSuffixTimeFormat.java
+++ src/java_impl/concreteTimeFormats/SingleSuffixTimeFormat.java
@@ -26,26 +26,41 @@ public class SingleSuffixTimeFormat extends TimeFormat {
 
     @Override
     public String formatSeconds(long seconds) {
+        if(seconds < 10) // basically turn 3s to 03s
+            return "0" + seconds + "s";
+
         return seconds + "s";
     }
 
     @Override
     public String formatMinutes(long minutes) {
+        if(minutes < 10)
+            return "0" + minutes + "m";
+
         return minutes + "m";
     }
 
     @Override
     public String formatHours(long hours) {
+        if(hours < 10)
+            return "0" + hours + "h";
+
         return hours + "h";
     }
 
     @Override
     public String formatDays(long days) {
+        if(days < 10)
+            return "0" + days + "d";
+
         return days + "d";
     }
 
     @Override
     public String formatMonths(long months) {
+        if(months < 10)
+            return "0" + months + "M";
+
         return months + "M";
     }
 
@@ -56,6 +71,7 @@ public class SingleSuffixTimeFormat extends TimeFormat {
 
     @Override
     public String getJoiningCharacter() {
+
         return ":";
     }
 
